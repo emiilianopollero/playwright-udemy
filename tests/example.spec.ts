20a4d0f51f68ec7e64de39be1c12b02851ac8754
emiilianopollero <emiliano.pollero.work@gmail.com>
2022-09-20 09:25:06 +0200

    More changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,40 @@ import {test, expect} from '@playwright/test'
 
 test("Simple basic test", async ({page}) => {
     await page.goto("https://es.wikipedia.org/wiki/Wikipedia:Portada")
-    const pageTitle = await page.locator("#n-mainpage-description > a > span")
+    const pageTitle = page.locator("#n-mainpage-description > a > span")
     await expect(pageTitle).toContainText("Portada")
+})
+
+test("Clickin on Element", async ({page}) => {
+    await page.goto("http://zero.webappsecurity.com/index.html")
+    await page.click("#signin_button")
+    await page.click("text=Sign in")
+    const errorMessage = await page.locator(".alert-error")
+    await expect(errorMessage).toContainText("Login and/or password are wrong.")
+})
+
+// test("Selectors",async ({page}) => {
+//     //text
+//     await page.click("text=some text selector")
+//     //CSS selectors
+//     await page.click("button")
+//     await page.click("#idSelector")
+//     await page.click(".classSelector")
+//     //Only visible CSS selector
+//     await page.click(".submit-button:visible")
+//     //Combinations
+//         //username + class
+//     await page.click("#username .first")
+//     //Xpath
+//     await page.click("//button")
+// })
+
+test("Working with inputs",async ({page}) => {
+    await page.goto("http://zero.webappsecurity.com/index.html")
+    await page.click("#signin_button")
+    await page.type("#user_login", "some username")
+    await page.type("#user_password", "some password")
+    await page.click("text=Sign in")
+    const errorMessage = await page.locator(".alert-error")
+    await expect(errorMessage).toContainText("Login and/or password are wrong.")
 })
